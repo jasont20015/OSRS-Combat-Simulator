03fa4b12913e0b13c3bd321c3f6c339f6141d2da
jason <jason@tavernier.nl>
2022-07-31 17:09:53 +0200

    Main setup

---
+++ C-rewrite/main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+struct Stat {
+    uint8_t baseLevel;
+    uint8_t visualLevel;
+};
+
+struct Player{
+    uint8_t attack_cycle;
+    Stat attack;
+    Stat strength;
+    Stat ranged;
+    Stat magic;
+};
+
+int main() {
+
+    return 0;
+}
---
+++ C-rewrite/main.h
@@ -0,0 +1,19 @@
+//
+// Created by Jason on 31/07/2022.
+//
+
+#ifndef C_REWRITE_MAIN_H
+#define C_REWRITE_MAIN_H
+
+#include <stdio.h>
+
+#include <stdlib.h>
+typedef struct Stat Stat;
+typedef struct Player Player;
+
+
+
+
+
+
+#endif //C_REWRITE_MAIN_H
